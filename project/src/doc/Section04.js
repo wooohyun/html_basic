91afdaa7650d0b559b3b990a2bdbc54532d1e812
wooohyun <woodoll543@gmail.com>
2022-08-08 17:34:46 +0900

    20220805

---
+++ project/src/doc/Section04.js
@@ -0,0 +1,27 @@
+const Section04 = () => {
+    return (
+        <section className="about02 basicSection">
+            <h2><span>CEO's</span> GREETINGS</h2>
+            <p>새로운 미래를 열어가는 우투드영투드우</p>
+            <div className="inner">
+                <div className="top">
+                    <figure>
+                        <img src="./img/main01.jpg" alt="" />
+                    </figure>
+                </div>
+                <div className="left">
+                    50%
+                </div>
+                <div className="right">
+                    50%
+                </div>
+
+
+            </div>
+
+
+
+        </section>
+    )
+}
+export default Section04;
